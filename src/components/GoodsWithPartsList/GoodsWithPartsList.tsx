3a49e157265eecff3c9d0f820c4af140cb1a4e0b
Osypiuk Dmytro <dimich578ua@gmail.com>
2022-03-30 10:56:02 +0300

    GoodsWithPartsList component fix

--- src/components/GoodsWithPartsList/GoodsWithPartsList.tsx
+++ src/components/GoodsWithPartsList/GoodsWithPartsList.tsx
@@ -8,30 +8,63 @@ type Props = {
   goods: ExportedData,
 };
 
+// const GoodsWithPartsList: React.FC<Props> = ({ goods }) => {
+//   let { item } = goods.items[0];
+//   item = item.filter((itemToFilter) => itemToFilter.parts);
+
+//   return (
+//     <div className="content">
+//       <PaginationList
+//         data={item}
+//         pageSize={5}
+//         renderItem={(renderedItem) => {
+//           const processedItem = renderedItem.$;
+
+//           return (
+//             <li key={processedItem.code}>
+//               {processedItem.name}
+
+//               <ul>
+//                 {item.parts && (
+//                   item.parts[0].part[0].item.map((part) => (
+//                     <li key={part.$.code}>
+//                       {part.$.name}
+//                     </li>
+//                   ))
+//                 )}
+//               </ul>
+//             </li>
+//           );
+//         }}
+//       />
+//     </div>
+//   );
+// };
+
+// export default GoodsWithPartsList;
+
 const GoodsWithPartsList: React.FC<Props> = ({ goods }) => {
   let { item } = goods.items[0];
-  item = item.filter((itemToFilter) => itemToFilter.parts);
+  item = item.filter((filteredItem) => filteredItem.parts);
 
   return (
     <div className="content">
       <PaginationList
         data={item}
         pageSize={5}
-        renderItem={(itemFromProps) => {
-          const processedItem = itemFromProps.$;
+        renderItem={(renderedItem) => {
+          const processedItem = renderedItem.$;
 
           return (
             <li key={processedItem.code}>
               {processedItem.name}
 
               <ul>
-                {item.parts && (
-                  item.parts[0].part[0].item.map((part) => (
-                    <li key={part.$.code}>
-                      {part.$.name}
-                    </li>
-                  ))
-                )}
+                {renderedItem.parts[0].part[0].item.map((part) => (
+                  <li key={part.$.code}>
+                    {part.$.name}
+                  </li>
+                ))}
               </ul>
             </li>
           );
